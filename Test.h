b571d36a8353547e34a75d1c6dead81a6f2a172c
Richard Hsu <rhsu@gwu.edu>
2015-08-18 23:32:37 -0400

    added Test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  GitTest
+//
+//  Created by Richard Hsu on 8/18/15.
+//  Copyright (c) 2015 Richard Hsu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  GitTest
+//
+//  Created by Richard Hsu on 8/18/15.
+//  Copyright (c) 2015 Richard Hsu. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
